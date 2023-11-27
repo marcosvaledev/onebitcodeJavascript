a89c81f1e30776e414668e6ee5cc5b0d253b39f5
Marcos Victor Sousa do Vale <111076997+marcosvaledev@users.noreply.github.com>
2023-11-27 15:53:07 -0300

    added functions

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,9 +1,18 @@
-let verificarOperador = prompt('what operation do you want calulate\n'+
-                               '+ for multiplication\n'+
-                               '- for subtration');
+let check = true;
 
+do {let verificarOperador = prompt('what operation do you want calulate\n'+
+                               '+ for adition\n'+
+                               '- for subtration\n' +
+                               '* for multiplication\n' +
+                               'sair for exit');
+
+if (verificarOperador.toLowerCase() === 'sair'){
+    break;
+}
+                           
 let value1 = Number(prompt('What number?'));
 let value2 = Number(prompt('What other number?'));
+
 
 function soma(value1, value2){
     return value1 + value2;
@@ -13,13 +22,24 @@ function subtrai(value1, value2){
     return value1 - value2;
 } let resultSubtrai = subtrai(value1, value2);
 
+function multipli(value1, value2){
+    return value1 * value2;
+} let resultMultipli = multipli(value1, value2);
+
+
+// the conditionals
 if (verificarOperador === '+'){
     alert('o resultado da soma é : ' + resultSoma.toFixed(1) );
 }
 else{if (verificarOperador === '-') {
     alert('o resultado da subtração é : ' + resultSubtrai.toFixed(1) )
 }
+}if (verificarOperador === '*') {
+    alert('o resultado da multiplication é : ' + resultMultipli.toFixed(1) )
 }
+} while(check === true)
+
+
 
 
 
