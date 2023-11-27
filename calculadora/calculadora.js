bb948bc73ff152a0c58d8143c5cbdc7d869dab4e
Marcos Victor Sousa do Vale <111076997+marcosvaledev@users.noreply.github.com>
2023-11-27 16:35:30 -0300

    feat: other ajusts

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,20 +1,20 @@
 let check = true;
 
-do {let verificarOperador = prompt('what operation do you want calulate\n'+
-                               '+ for adition\n'+
-                               '- for subtration\n' +
-                               '* for multiplication\n' +
-                               'sair for exit');
+do {
+let verificarOperador = prompt('what operation do you want calulate\n'+
+                                   '+ for adition\n'+
+                                   '- for subtration\n' +
+                                   '* for multiplication\n' +
+                                   'sair for exit');
 
-if (verificarOperador.toLowerCase() === 'sair'){
+if (verificarOperador.toLowerCase() === 'sair') {
+    check = false;
     break;
 }
-                           
 let value1 = Number(prompt('What number?'));
 let value2 = Number(prompt('What other number?'));
 
-
-function soma(value1, value2){
+function soma(value1, value2){         
     return value1 + value2;
 } let resultSoma = soma(value1, value2);
 
@@ -26,36 +26,19 @@ function multipli(value1, value2){
     return value1 * value2;
 } let resultMultipli = multipli(value1, value2);
 
-
 // the conditionals
 if (verificarOperador === '+'){
     alert('o resultado da soma é : ' + resultSoma.toFixed(1) );
 }
-else{if (verificarOperador === '-') {
+else if (verificarOperador === '-'){ {
     alert('o resultado da subtração é : ' + resultSubtrai.toFixed(1) )
 }
-}if (verificarOperador === '*') {
+}else if(verificarOperador === '*') {
     alert('o resultado da multiplication é : ' + resultMultipli.toFixed(1) )
+}else if (verificarOperador != '+' && '-' && '*' && 'sair' ){
+    alert('operation invalid')
 }
-} while(check === true)
+
+} while(check);
 
 
-
-
-
-
-
-
-
-
-
-
-//tem outra forma tambem
-
-/*const soma = x + y
-const subtracao = x - y
-const multiplicacao = x * y
-const divisao = x / y
-*/
-
-//depois exibir um alert com as varias  concatenadas
