feafe99240b5ff4630c2789cbca2c10fa30effee
Marcos Victor Sousa do Vale <111076997+marcosvaledev@users.noreply.github.com>
2023-12-07 11:19:20 -0300

    feat: complete calcule damaged

--- calculeDano/index.js
+++ calculeDano/index.js
@@ -4,18 +4,25 @@
 
 
 let nomePlayer = prompt('qual seu nome?');
-let powerAtack = Number(prompt('what its atack?'));
+let powerAtack = Number(prompt('what its power atack?'));
 
 const defender = prompt('name of defender?');
 let lifePoints = Number(prompt('what lifes points?'));
 const powerDefender = Number(prompt('what power Denfender?'));
 let shield = prompt('he have shield? Yes or No?');
-
+let damageCaused = 0;
 //se o poder de ataque for maior que defesa e o defensor nao possui escuto
 //entao o dano causado sera igual a diferença entre ataque e defesa
 
-if powerAtack > powerDefender && shield === "No"
+if (powerAtack > powerDefender && shield === "no"){
+    damageCaused = powerAtack - powerDefender;
+}
 //se o poder de ataque for maior que a defesa E o defensor possui escuto
 //entao o dano causado sera igual a metade da diferença entre o atauqe e defesa
-
+else if(powerAtack > powerDefender && shield === 'yes'){
+    damageCaused = (powerAtack - powerDefender) / 2;
+}
 //se o poder de ataque for menor ou igual a defesa, o dano causado sera 0
+lifePoints -= damageCaused; // é subtraido esse valor pois o danocausado no poder de defesa, faz com que ele perca os pontos de vida (lifePoints).
+
+alert()
